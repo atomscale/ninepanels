ad2fcc7cba1e6c25d91bd6059d0bd07b71a5a2b4
Ben Dyer <ben@atomscale.co>
2023-08-21 15:03:42 +0100

    remove sqlite code, move Base to sqlmodels

--- ninepanels/database.py
+++ ninepanels/database.py
@@ -1,33 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import event
-from sqlite3 import Connection as SQLiteConnection
 
 from .config import SQLALCHEMY_DATABASE_URI
 
 
-if "sqlite" in SQLALCHEMY_DATABASE_URI:
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, pool_timeout=30
-    )
-
-
-@event.listens_for(engine, "connect")
-def _set_fk_pragma(conn, conn_record):
-    if isinstance(conn, SQLiteConnection):
-        cur = conn.cursor()
-        cur.execute("pragma foreign_keys=1;")
-        cur.close()
-
+engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, pool_timeout=30)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
