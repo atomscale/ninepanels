fc6ffd255c013b3efca60d41f17e262181a9280b
Ben Dyer <ben@atomscale.co>
2023-08-23 10:44:30 +0100

    full data only backup and restore process

--- restore.sh
+++ restore.sh
@@ -2,44 +2,71 @@
 echo "\e[1;34mRestore...\e[0m"
 echo
 
-echo "this restore will affect the $NINEPANELS_ENV database, replacing data there. Continue?"
+echo "this restore will affect the \033[1;32m$NINEPANELS_ENV\033[0m database, replacing data there. Continue?"
 select restore_opt in "Yes" "No"; do
     case $restore_opt in
         "Yes")
-            echo "supply path to dump file"
-            read dump_file_path
+            select restore_type in "Full" "Data only"; do
+                case $restore_type in
+                    "Full")
+                        echo "supply path to dump file"
+                        read dump_file_path
 
-            echo "restoring $NINEPANELS_ENV database from $dump_file_path"
+                        echo "restoring $NINEPANELS_ENV database from $dump_file_path"
 
-            # establish a timestamp to id this process
-            clone_ts=$(date +"%Y%m%d%H%M%S")
+                        # establish a timestamp to id this process
+                        clone_ts=$(date +"%Y%m%d%H%M%S")
 
-            echo "Taking pre-restore backup..."
-            pg_dump --no-owner --no-acl -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres -t alembic_version -t users -t panels -t entries -F c >"/Users/bd/Library/CloudStorage/GoogleDrive-ben@atomscale.co/My Drive/databases/ninepanels/${clone_ts}_${NINEPANELS_ENV}.dump"
+                        echo "Taking pre-restore backup..."
+                        pg_dump --no-owner --no-acl -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres -t alembic_version -t users -t panels -t entries -F c >"/Users/bd/Library/CloudStorage/GoogleDrive-ben@atomscale.co/My Drive/databases/ninepanels/${clone_ts}_${NINEPANELS_ENV}.dump"
 
-            echo "Pre-restore backup of target complete."
-            echo
-
-
-            echo "Dropping target database..."
-            dropdb --if-exists -U postgres -h $DB_HOSTNAME -p $DB_PORT postgres
-            if [ $? -ne 0 ]; then
-                echo "error occurred dropping db..."
-                return
-            fi
-            echo "Target database dropped."
-            echo
-
-            echo "Recreating target database..."
-            createdb -U postgres -h $DB_HOSTNAME -p $DB_PORT postgres
-            echo "Target database recreated"
-            echo
-
-            echo "Restoring data from the source file..."
-            pg_restore --no-owner -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres "$dump_file_path"
-            echo "Target database restored from source dump."
+                        echo "Pre-restore backup of target complete."
+                        echo
 
 
+                        echo "Dropping target database..."
+                        dropdb --if-exists -U postgres -h $DB_HOSTNAME -p $DB_PORT postgres
+                        if [ $? -ne 0 ]; then
+                            echo "error occurred dropping db..."
+                            return
+                        fi
+                        echo "Target database dropped."
+                        echo
+
+                        echo "Recreating target database..."
+                        createdb -U postgres -h $DB_HOSTNAME -p $DB_PORT postgres
+                        echo "Target database recreated"
+                        echo
+
+                        echo "Restoring data from the source file..."
+                        pg_restore --no-owner -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres "$dump_file_path"
+                        echo "Target database restored from source dump."
+
+
+                        break
+                        ;;
+                    "Data only")
+                        echo "supply path to dump file"
+                        read dump_file_path
+
+                        echo "restoring $NINEPANELS_ENV database from $dump_file_path"
+
+                        # establish a timestamp to id this process
+                        clone_ts=$(date +"%Y%m%d%H%M%S")
+
+                        echo "Taking pre-restore backup..."
+                        pg_dump --no-owner --no-acl -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres -t alembic_version -t users -t panels -t entries -F c >"/Users/bd/Library/CloudStorage/GoogleDrive-ben@atomscale.co/My Drive/databases/ninepanels/${clone_ts}_${NINEPANELS_ENV}.dump"
+
+                        echo "Pre-restore backup of target complete."
+                        echo
+
+                        echo "Restoring data from the source file..."
+                        psql -U postgres -h $DB_HOSTNAME -p $DB_PORT -d postgres < "$dump_file_path"
+                        echo "Target database restored from source dump."
+                        break
+                        ;;
+                    esac
+                done
             break
             ;;
         "No")
