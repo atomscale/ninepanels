546dbc817fa938b504ad6cda1a85fef5a6849679
Ben Dyer <ben@atomscale.co>
2023-08-29 10:04:02 +0100

    consistency algo scaffold

--- tests/scratch/consistency_metric.py
+++ tests/scratch/consistency_metric.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone, timedelta
 from pprint import PrettyPrinter
-import pandas as pd
+
 
 pp = PrettyPrinter()
 
@@ -9,43 +9,159 @@ panels = [
         "id": 1,
         "position": 0,
         "title": "A",
-        "created_at": "2023-08-01T23:59:59.1",
+        "created_at": datetime(2023, 8, 1, 13),
         "entries": [
             {
-                "id": 7,
+                "id": 5,
                 "is_complete": True,
-                "timestamp": "2023-08-01T18:00:00.0",
+                "timestamp": datetime(2023, 8, 1, 18, 3),
+                "panel_id": 1,
+            },
+            {
+                "id": 7,
+                "is_complete": False,
+                "timestamp": datetime(2023, 8, 1, 18),
                 "panel_id": 1,
             },
             {
                 "id": 8,
                 "is_complete": True,
-                "timestamp": "2023-08-02T18:00:00.0",
+                "timestamp": datetime(2023, 8, 1, 14),
                 "panel_id": 1,
             },
             {
                 "id": 11,
-                "is_complete": False,
-                "timestamp": "2023-08-02T19:00:00.0",
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 4, 22),
                 "panel_id": 1,
             },
             {
                 "id": 14,
                 "is_complete": True,
-                "timestamp": "2023-08-05T18:00:00.0",
+                "timestamp": datetime(2023, 8, 10, 13),
                 "panel_id": 1,
             },
             {
                 "id": 98,
                 "is_complete": True,
-                "timestamp": "2023-08-09T18:00:00.0",
+                "timestamp": datetime(2023, 8, 11, 13),
                 "panel_id": 1,
             },
             {
                 "id": 99,
                 "is_complete": False,
-                "timestamp": "2023-08-09T19:00:00.0",
+                "timestamp": datetime(2023, 8, 11, 13, 0, 1),
                 "panel_id": 1,
+            },
+            {
+                "id": 1987,
+                "is_complete": True,
+                "timestamp": datetime.utcnow(),
+                "panel_id": 1,
+            },
+
+        ],
+    },
+    {
+        "id": 2,
+        "position": 1,
+        "title": "B",
+        "created_at": datetime(2023, 8, 20, 13),
+        "entries": [
+            {
+                "id": 5,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 20, 18, 3),
+                "panel_id": 2,
+            },
+            {
+                "id": 6,
+                "is_complete": False,
+                "timestamp": datetime(2023, 8, 20, 18, 3, 25),
+                "panel_id": 2,
+            },
+            {
+                "id": 7,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 20, 18, 3, 25, 400),
+                "panel_id": 2,
+            },
+            {
+                "id": 8,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 21, 14),
+                "panel_id": 2,
+            },
+            {
+                "id": 11,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 22, 22),
+                "panel_id": 2,
+            },
+            {
+                "id": 14,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 24, 13),
+                "panel_id": 2,
+            },
+            {
+                "id": 98,
+                "is_complete": True,
+                "timestamp": datetime(2023, 8, 25, 13),
+                "panel_id": 2,
+            },
+            {
+                "id": 99,
+                "is_complete": False,
+                "timestamp": datetime(2023, 8, 25, 14, 1),
+                "panel_id": 2,
+            },
+            {
+                "id": 1987,
+                "is_complete": True,
+                "timestamp": datetime.utcnow() + timedelta(hours=-1),
+                "panel_id": 2,
+            },
+            {
+                "id": 1987,
+                "is_complete": False,
+                "timestamp": datetime.utcnow(),
+                "panel_id": 2,
+            },
+
+        ],
+    },
+    {
+        "id": 3,
+        "position": 2,
+        "title": "Created today",
+        "created_at": datetime.utcnow() + timedelta(minutes=-2),
+        "entries": [
+            {
+                "id": 534531,
+                "is_complete": True,
+                "timestamp": datetime.utcnow(),
+                "panel_id": 3,
+            },
+        ],
+    },
+    {
+        "id": 4,
+        "position": 3,
+        "title": "Created yesterday and completed yesterdy and today",
+        "created_at": datetime.utcnow() + timedelta(days=-1),
+        "entries": [
+            {
+                "id": 534531,
+                "is_complete": True,
+                "timestamp": datetime.utcnow() + timedelta(days=-1),
+                "panel_id": 4,
+            },
+            {
+                "id": 534531,
+                "is_complete": True,
+                "timestamp": datetime.utcnow(),
+                "panel_id": 4,
             },
         ],
     }
@@ -53,24 +169,60 @@ panels = [
 
 # pp.pprint(panels)
 
+
 def today() -> datetime:
     today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     return today
 
-def panel_age(created_at: datetime) -> int:
-    panel_age = (today() - datetime.fromisoformat(created_at))
-    return panel_age.days + 1
+
+def calc_panel_age(created_at: datetime) -> int:
+    panel_age = today() - created_at
+    return panel_age.days + 2
+
 
 print()
 for panel in panels:
-    print(f"Panel {panel['title']}:")
+    print(f"Panel '{panel['title']}':")
 
-    panel_age = panel_age(created_at=panel['created_at'])
+    panel_age = calc_panel_age(created_at=panel["created_at"])
     print(f"{panel_age=}")
 
+    date_range = []
+    start_date = panel["created_at"]
+    start_date = start_date.replace(hour=23, minute=59, second=59, microsecond=100000)
 
-    date_range = pd.date_range(datetime.fromisoformat(panel['created_at']), datetime.now(), freq="D")
+    date_range = []
+    date_range.append(start_date)
 
+    day_counter = 0
+
+    for i in range(panel_age):
+        day_counter += 1
+        new_date = start_date + timedelta(days=day_counter)
+        date_range.append(new_date)
+
+    # pp.pprint(date_range)
+
+    complete_counter = 0
     for date in date_range:
-        print(date)
+        day_matches = []
+
+
+        for entry in panel["entries"]:
+            if date.day == entry["timestamp"].day:
+                day_matches.append(entry)
+
+        if day_matches:
+            sorted_day_match = sorted(day_matches, key=lambda x: x['timestamp'], reverse=True)
+            if sorted_day_match[0]['is_complete'] == True:
+                complete_counter += 1
+
+    if panel_age > 0:
+        panel_consistency = complete_counter/panel_age
+    else:
+        panel_consistency = 0
+    print(f"{complete_counter=}")
+    print(f"consistency for panel '{panel['title']}': {panel_consistency}")
+    print()
+
 
