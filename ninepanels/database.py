3161c83a44e92210a56345b097e21f0fc0b4424c
Ben Dyer <ben@atomscale.co>
2023-10-04 12:16:29 +0100

    simple pool monitoring

--- ninepanels/database.py
+++ ninepanels/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import event
 from sqlalchemy import text # keep, imported eslewhere
 from sqlalchemy.orm import sessionmaker
+import rollbar
 
 from . import config
 
@@ -10,11 +11,31 @@ engine = create_engine(
     config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, pool_timeout=30
 )
 
+in_alert = False
+
 def see_status_checkout(dbapi_con, con_record, con_proxy):
-    print(engine.pool.status())
+    global in_alert
+    msg = engine.pool.status()
+    num_connections = int(msg[-1])
+    alert_threshold = 6
+    if num_connections > alert_threshold:
+        if not in_alert:
+            in_alert = True
+            rollbar.report_message(message=f"db connection pool > {alert_threshold}", level='warn')
+    else:
+        in_alert = False
 
 def see_status_checkin(dbapi_con, con_record):
-    print(engine.pool.status())
+    global in_alert
+    msg = engine.pool.status()
+    num_connections = int(msg[-1])
+    alert_threshold = 6
+    if num_connections > alert_threshold:
+        if not in_alert:
+            in_alert = True
+            rollbar.report_message(message=f"db connection pool > {alert_threshold}", level='warn')
+    else:
+        in_alert = False
 
 event.listen(engine.pool, 'checkout', see_status_checkout)
 event.listen(engine.pool, 'checkin', see_status_checkin)
